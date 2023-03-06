16d85b20fe04c2df56ce758ccb5df28471291890
Jonathan Demaree <jonathan136@revature.net>
2023-03-06 12:49:02 -0800

    Base Spring Boot App

---
+++ project2-backend/src/main/java/com/revature/project2backend/Project2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.project2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2BackendApplication.class, args);
+	}
+
+}
---
+++ project2-backend/src/test/java/com/revature/project2backend/Project2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.project2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
